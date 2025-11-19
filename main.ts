f2155e84f5636dae7030198ef8ace0d49cae74ed
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2025-11-19 22:35:22 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 import { S3Client, GetObjectCommand } from "npm:@aws-sdk/client-s3";
 import { getSignedUrl } from "npm:@aws-sdk/s3-request-presigner";
 
-const R2_ACCOUNT_ID = "YOUR_ACCOUNT_ID_HERE";
-const ACCESS_KEY_ID = "YOUR_ACCESS_KEY_HERE";
-const SECRET_ACCESS_KEY = "YOUR_SECRET_KEY_HERE";
-const BUCKET_NAME = "YOUR_BUCKET_NAME_HERE";
+const R2_ACCOUNT_ID = Deno.env.get("R2_ACCOUNT_ID");
+const ACCESS_KEY_ID = Deno.env.get("R2_ACCESS_KEY_ID");
+const SECRET_ACCESS_KEY = Deno.env.get("R2_SECRET_ACCESS_KEY");
+const BUCKET_NAME = Deno.env.get("R2_BUCKET_NAME");
+
+if (!R2_ACCOUNT_ID || !ACCESS_KEY_ID || !SECRET_ACCESS_KEY || !BUCKET_NAME) {
+  console.error("Error: Environment variables are missing!");
+}
 
 const S3 = new S3Client({
   region: "auto",
   endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY,
+    accessKeyId: ACCESS_KEY_ID!,
+    secretAccessKey: SECRET_ACCESS_KEY!,
   },
 });
 
@@ -29,12 +33,12 @@ Deno.serve(async (req) => {
       Key: fileName,
     });
 
-    // 3 Hours = 10800 Seconds
+    // 10800 seconds = 3 hours
     const signedUrl = await getSignedUrl(S3, command, { expiresIn: 10800 });
 
     return Response.redirect(signedUrl, 302);
 
   } catch (error) {
-    return new Response("File not found", { status: 404 });
+    return new Response("File not found or Error", { status: 404 });
   }
 });
