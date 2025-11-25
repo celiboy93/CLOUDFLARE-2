9acce688b76e769dd5c5133b797603f47121e8b6
celiboy93 <97116174+celiboy93@users.noreply.github.com>
2025-11-26 01:17:07 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -195,7 +195,7 @@ Deno.serve(async (req: Request) => {
                 ContentDisposition: `attachment; filename="${fileName}"`,
                 CacheControl: "public, max-age=31536000, immutable"
             },
-            queueSize: 8, partSize: 50 * 1024 * 1024 // Increased to 50MB
+            queueSize: 4, partSize: 30 * 1024 * 1024 // Increased to 50MB
           });
           upload.on("httpUploadProgress", p => { if(total) push({progress:Math.round((p.loaded!/total)*100)}) });
           await upload.done();
